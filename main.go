7b4fe61e3d2c409efcea2e79d82003d3aa38641d
eslerkang <eslerkang@gmail.com>
2022-02-06 16:13:28 +0000

    REFACTOR: added AllBlocks method and changed block struct's thing public

--- main.go
+++ main.go
@@ -8,5 +8,10 @@ import (
 
 func main() {
 	chain := blockchain.GetBlockChain()
-	fmt.Println(chain)
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\nHash: %s\nPrevHash: %s\n\n", block.Data, block.Hash, block.PrevHash)
+	}
 }
