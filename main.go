4b0bfdb69b914a8317dc9728bffdabb7a4be58b8
eslerkang <eslerkang@gmail.com>
2022-02-07 11:59:22 +0000

    REFACTOR: make explorer and rest api run simultaneously

--- main.go
+++ main.go
@@ -1,79 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/eslerkang/tjcoin/blockchain"
-	"github.com/eslerkang/tjcoin/utils"
+	"github.com/eslerkang/tjcoin/explorer"
+	"github.com/eslerkang/tjcoin/rest"
 )
 
-const port string =":4000"
-
-type URL string
-
-type URLDescription struct {
-	URL URL `json:"url"`
-	Method string `json:"method"`
-	Description string `json:"description"`
-	Payload string `json:"payload,omitempty"`
-}
-
-type AddBlockBody struct {
-	Data string
-}
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription {
-		{
-			URL: URL("/"),
-			Method: "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL: URL("/blocks"),
-			Method: "GET",
-			Description: "GET All Block",
-		},
-		{
-			URL: URL("/blocks"),
-			Method: "POST",
-			Description: "Add A Block",
-			Payload: "data:string",
-		},
-		{
-			URL: URL("/blocks/{id}"),
-			Method: "GET",
-			Description: "See A Block",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "json")
-		json.NewEncoder(rw).Encode(blockchain.GetBlockChain().AllBlocks())
-	case "POST":
-		var addBlockBody AddBlockBody
-		utils.HandleError(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		blockchain.GetBlockChain().AddBlock(addBlockBody.Data)
-		rw.WriteHeader(http.StatusCreated)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))	
+	go explorer.Start(3000)
+	rest.Start(4000)
 }
---
+++ rest/rest.go
@@ -0,0 +1,81 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/eslerkang/tjcoin/blockchain"
+	"github.com/eslerkang/tjcoin/utils"
+)
+
+var port string
+
+type url string
+
+type urlDescription struct {
+	URL url `json:"url"`
+	Method string `json:"method"`
+	Description string `json:"description"`
+	Payload string `json:"payload,omitempty"`
+}
+
+type addBlockBody struct {
+	Data string
+}
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription {
+		{
+			URL: url("/"),
+			Method: "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL: url("/blocks"),
+			Method: "GET",
+			Description: "GET All Block",
+		},
+		{
+			URL: url("/blocks"),
+			Method: "POST",
+			Description: "Add A Block",
+			Payload: "data:string",
+		},
+		{
+			URL: url("/blocks/{id}"),
+			Method: "GET",
+			Description: "See A Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockChain().AllBlocks())
+	case "POST":
+		var addBlockBody addBlockBody
+		utils.HandleError(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockChain().AddBlock(addBlockBody.Data)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/", documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, handler))	
+}
