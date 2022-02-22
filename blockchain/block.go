764f7f156a8deba08767b9873297d482a1008c59
eslerkang <eslerkang@gmail.com>
2022-02-22 18:01:05 +0900

    FEAT: created a simple Proof of Work

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,16 +4,21 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/eslerkang/tjcoin/db"
 	"github.com/eslerkang/tjcoin/utils"
 )
 
+const difficulty int = 2
+
 type Block struct {
-	Data     string `json:"data"`
-	Hash     string `json:"hash"`
-	PrevHash string `json:"prevHash,omitempty"`
-	Height   int    `json:"height"`
+	Data       string `json:"data"`
+	Hash       string `json:"hash"`
+	PrevHash   string `json:"prevHash,omitempty"`
+	Height     int    `json:"height"`
+	Difficulty int    `json:"difficulty"`
+	Nonce      int    `json:"nonce"`
 }
 
 func (b *Block) persist() {
@@ -36,16 +41,32 @@ func FindBlock(hash string) (*Block, error) {
 	return block, nil
 }
 
+func (b *Block) mine() {
+	target := strings.Repeat("0", b.Difficulty)
+	for {
+		blockAsString := fmt.Sprint(b)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String: %s\nHash: %s\nTarget: %s\nNonce: %d\n\n",
+			blockAsString, hash, target, b.Nonce)
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 func createBlock(data string) *Block {
 	block := Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: BlockChain().NewestHash,
-		Height:   BlockChain().Height + 1,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   BlockChain().NewestHash,
+		Height:     BlockChain().Height + 1,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
-
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(payload)))
+	block.mine()
 	block.persist()
 	return &block
 }
